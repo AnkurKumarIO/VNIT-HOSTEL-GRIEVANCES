f78649b98020cd79318e3e04f3cd0438d189b8c0
ANKUR KR. <professionaly.ak@gmail.com>
2025-11-15 08:13:35 +0530

    Update API_URL to new endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://vnit-hostel-grievances-1-o.onrender.com';
+const API_URL = 'https://vnit-hostel-grievances.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
